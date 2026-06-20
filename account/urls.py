f3a8554fde5fce35b65ca3cf80ba9336aa1814e7
chistev <stephenowabie@gmail.com>
2026-06-20 10:46:00 +0100

    fixed the login and register layout problem where the containers were not centered by adding the login-page class dynamically; fixed problem where user was not redirected to home page after logging in successfully; implemented logout functionality; index html extends base template

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'account'
 urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login_view, name='login'),
+    path('logout', LogoutView.as_view(next_page='home', http_method_names=['post']), name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -30,6 +30,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            return redirect('home')
         else:
             messages.error(request, "Invalid username or password.")
     else:
