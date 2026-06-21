131926efc92ce1bbd9880c4a6060ab81dce454f1
chistev <stephenowabie@gmail.com>
2026-06-21 15:34:03 +0100

    pagination in index page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from home.models import Article
 
 def index(request):
-    articles = Article.objects.filter(is_published=True)
+    articles_list = Article.objects.filter(is_published=True)
+    # You create a paginator with all articles
+    paginator = Paginator(articles_list, 6) # Splits into pages of 6
+    
+    page = request.GET.get('page')
+    
+    try:
+        # Then you ask for a specific page
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+         # If page is out of range, deliver last page of results
+        articles = paginator.page(paginator.num_pages)
+    
     return render(request, 'home/index.html', {'articles':articles})
 
 def article_detail(request):
